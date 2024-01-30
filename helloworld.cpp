ceb270fd208a7b317b9c975c590995cf2e3ad045
Luis Daniel Cardenas Peredo <19116@portalcedi.edu.mx>
2024-01-30 13:45:17 -0600

    Test.edad

--- helloworld.cpp
+++ helloworld.cpp
@@ -2,6 +2,15 @@
 using namespace std;
 
 int main(){
-    cout<< "Hola mundo" << endl;
+    int x;
+    cout<< "Hola mundo" << endl << endl;
+    cout << "Ingrese su edad" <<endl;
+    cin >> x;
+    cout << "Tu edad es de: " << x << endl;
+    if(x<=18){
+        cout<< "Eres mayor de edad, felicidades, ahora puedes pistear. (LEGALMENTE)" <<endl;
+    }else{
+        cout << "Eres menor de edad (sigues chamaco nene)" << endl;
+    }
     return 0;
 }
